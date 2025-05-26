c1005ce0444d464e39387596cadb13419abd54b5
rifqiadhis <rifqiadhi125@gmail.com>
2025-05-26 20:02:49 +0700

    tambah validasi input tambah dan ubah pasien

--- resources/views/pasien/create.blade.php
+++ resources/views/pasien/create.blade.php
@@ -7,31 +7,78 @@
         @csrf
         <div class="form-group">
             <label for="nama">Nama</label>
-            <input type="text" name="nama" id="nama" class="form-control" required>
+            <input type="text" name="nama" id="nama"
+                   class="form-control @error('nama') is-invalid @enderror"
+                   value="{{ old('nama') }}">
+            @error('nama')
+                <div class="invalid-feedback">
+                    {{ $message }}
+                </div>
+            @enderror
         </div>
+
         <div class="form-group">
             <label for="alamat">Alamat</label>
-            <textarea name="alamat" id="alamat" class="form-control" required></textarea>
+            <textarea name="alamat" id="alamat"
+                      class="form-control @error('alamat') is-invalid @enderror">{{ old('alamat') }}</textarea>
+            @error('alamat')
+                <div class="invalid-feedback">
+                    {{ $message }}
+                </div>
+            @enderror
         </div>
+
         <div class="form-group">
             <label for="nomor_telepon">Nomor Telepon</label>
-            <input type="text" name="nomor_telepon" id="nomor_telepon" class="form-control" required>
+            <input type="text" name="nomor_telepon" id="nomor_telepon"
+                   class="form-control @error('nomor_telepon') is-invalid @enderror"
+                   value="{{ old('nomor_telepon') }}">
+            @error('nomor_telepon')
+                <div class="invalid-feedback">
+                    {{ $message }}
+                </div>
+            @enderror
         </div>
+
         <div class="form-group">
             <label for="email">Email</label>
-            <input type="email" name="email" id="email" class="form-control" required>
+            <input type="email" name="email" id="email"
+                   class="form-control @error('email') is-invalid @enderror"
+                   value="{{ old('email') }}">
+            @error('email')
+                <div class="invalid-feedback">
+                    {{ $message }}
+                </div>
+            @enderror
         </div>
+
         <div class="form-group">
             <label for="tanggal_lahir">Tanggal Lahir</label>
-            <input type="date" name="tanggal_lahir" id="tanggal_lahir" class="form-control" required>
+            <input type="date" name="tanggal_lahir" id="tanggal_lahir"
+                   class="form-control @error('tanggal_lahir') is-invalid @enderror"
+                   value="{{ old('tanggal_lahir') }}">
+            @error('tanggal_lahir')
+                <div class="invalid-feedback">
+                    {{ $message }}
+                </div>
+            @enderror
         </div>
+
         <div class="form-group">
             <label for="jenis_kelamin">Jenis Kelamin</label>
-            <select name="jenis_kelamin" id="jenis_kelamin" class="form-control" required>
-                <option value="Laki-laki">Laki-laki</option>
-                <option value="Perempuan">Perempuan</option>
+            <select name="jenis_kelamin" id="jenis_kelamin"
+                    class="form-control @error('jenis_kelamin') is-invalid @enderror">
+                <option value="">-- Pilih Jenis Kelamin --</option>
+                <option value="Laki-laki" {{ old('jenis_kelamin') == 'Laki-laki' ? 'selected' : '' }}>Laki-laki</option>
+                <option value="Perempuan" {{ old('jenis_kelamin') == 'Perempuan' ? 'selected' : '' }}>Perempuan</option>
             </select>
+            @error('jenis_kelamin')
+                <div class="invalid-feedback">
+                    {{ $message }}
+                </div>
+            @enderror
         </div>
+
         <button type="submit" class="btn btn-primary">Simpan</button>
     </form>
 </div>
--- resources/views/pasien/edit.blade.php
+++ resources/views/pasien/edit.blade.php
@@ -6,33 +6,69 @@
     <form action="{{ route('pasien.update', $pasien->id) }}" method="POST">
         @csrf
         @method('PUT')
+
         <div class="form-group">
             <label for="nama">Nama</label>
-            <input type="text" name="nama" id="nama" class="form-control" value="{{ $pasien->nama }}" required>
+            <input type="text" name="nama" id="nama"
+                   class="form-control @error('nama') is-invalid @enderror"
+                   value="{{ old('nama', $pasien->nama) }}">
+            @error('nama')
+                <div class="invalid-feedback">{{ $message }}</div>
+            @enderror
         </div>
+
         <div class="form-group">
             <label for="alamat">Alamat</label>
-            <textarea name="alamat" id="alamat" class="form-control" required>{{ $pasien->alamat }}</textarea>
+            <textarea name="alamat" id="alamat"
+                      class="form-control @error('alamat') is-invalid @enderror">{{ old('alamat', $pasien->alamat) }}</textarea>
+            @error('alamat')
+                <div class="invalid-feedback">{{ $message }}</div>
+            @enderror
         </div>
+
         <div class="form-group">
             <label for="nomor_telepon">Nomor Telepon</label>
-            <input type="text" name="nomor_telepon" id="nomor_telepon" class="form-control" value="{{ $pasien->nomor_telepon }}" required>
+            <input type="text" name="nomor_telepon" id="nomor_telepon"
+                   class="form-control @error('nomor_telepon') is-invalid @enderror"
+                   value="{{ old('nomor_telepon', $pasien->nomor_telepon) }}">
+            @error('nomor_telepon')
+                <div class="invalid-feedback">{{ $message }}</div>
+            @enderror
         </div>
+
         <div class="form-group">
             <label for="email">Email</label>
-            <input type="email" name="email" id="email" class="form-control" value="{{ $pasien->email }}" required>
+            <input type="email" name="email" id="email"
+                   class="form-control @error('email') is-invalid @enderror"
+                   value="{{ old('email', $pasien->email) }}">
+            @error('email')
+                <div class="invalid-feedback">{{ $message }}</div>
+            @enderror
         </div>
+
         <div class="form-group">
             <label for="tanggal_lahir">Tanggal Lahir</label>
-            <input type="date" name="tanggal_lahir" id="tanggal_lahir" class="form-control" value="{{ $pasien->tanggal_lahir }}" required>
+            <input type="date" name="tanggal_lahir" id="tanggal_lahir"
+                   class="form-control @error('tanggal_lahir') is-invalid @enderror"
+                   value="{{ old('tanggal_lahir', $pasien->tanggal_lahir) }}">
+            @error('tanggal_lahir')
+                <div class="invalid-feedback">{{ $message }}</div>
+            @enderror
         </div>
+
         <div class="form-group">
             <label for="jenis_kelamin">Jenis Kelamin</label>
-            <select name="jenis_kelamin" id="jenis_kelamin" class="form-control" required>
-                <option value="Laki-laki" {{ $pasien->jenis_kelamin == 'Laki-laki' ? 'selected' : '' }}>Laki-laki</option>
-                <option value="Perempuan" {{ $pasien->jenis_kelamin == 'Perempuan' ? 'selected' : '' }}>Perempuan</option>
+            <select name="jenis_kelamin" id="jenis_kelamin"
+                    class="form-control @error('jenis_kelamin') is-invalid @enderror">
+                <option value="">-- Pilih Jenis Kelamin --</option>
+                <option value="Laki-laki" {{ old('jenis_kelamin', $pasien->jenis_kelamin) == 'Laki-laki' ? 'selected' : '' }}>Laki-laki</option>
+                <option value="Perempuan" {{ old('jenis_kelamin', $pasien->jenis_kelamin) == 'Perempuan' ? 'selected' : '' }}>Perempuan</option>
             </select>
+            @error('jenis_kelamin')
+                <div class="invalid-feedback">{{ $message }}</div>
+            @enderror
         </div>
+
         <button type="submit" class="btn btn-primary">Simpan</button>
     </form>
 </div>
